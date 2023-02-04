458109ef12c72f58557f8510bd26cc936be6c783
Kyle Hanson <khanson1029@gmail.com>
2023-02-04 16:01:13 -0700

    Fat Jar, Build and Test scripts done

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-exit 127
+
+# Run the gradle task to build the fat JAR file
+cd antscuttle
+
+./gradlew desktop:dist
+
+# Get the name of the JAR file that was created
+JAR_FILE=$(find . -name "*.jar" | grep "antscuttle" | tail -1)
+
+# Copy the JAR file to the directory above
+cp "$JAR_FILE" "../${JAR_FILE##*/}"
+
+echo "Fat JAR file copied to $(cd .. && pwd)/${JAR_FILE##*/}"
