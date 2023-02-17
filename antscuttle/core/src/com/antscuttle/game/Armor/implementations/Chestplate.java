be118e3e5baf93eedb20d1c4aa0ed290890f2adc
anthonyoffire <anthonybaird@protonmail.com>
2023-02-17 14:17:32 -0700

    implemented Chestplate

---
+++ antscuttle/core/src/com/antscuttle/game/Armor/implementations/Chestplate.java
@@ -0,0 +1,14 @@
+/*
+ * Chestplate class
+ */
+package com.antscuttle.game.Armor.implementations;
+import com.antscuttle.game.Armor.Armor;
+/**
+ *
+ * @author antho
+ */
+public class Chestplate extends Armor{
+    private String name = "Chestplate";
+    private int defense = 10;
+    private String img = "";
+}
