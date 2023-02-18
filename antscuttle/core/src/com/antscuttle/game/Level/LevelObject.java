e04e6d3bbc778c489461bcd99a8706a64072a479
Kyle Hanson <khanson1029@gmail.com>
2023-02-18 13:56:49 -0700

    LevelObject Class

---
+++ antscuttle/core/src/com/antscuttle/game/Level/LevelObject.java
@@ -0,0 +1,24 @@
+package com.antscuttle.game.Level;
+import com.antscuttle.game.Damage.DamageType;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public abstract class LevelObject extends Actor {
+    // Fields
+    protected Texture texture;
+    protected int defense;
+
+    // Constructor
+    public LevelObject(Texture texture, int defense) {
+        this.texture = texture;
+        setWidth(texture.getWidth());
+        setHeight(texture.getHeight());
+        this.defense = defense;
+    }
+
+    // Methods
+    public abstract int attack(int damage, DamageType damageType);
+
+    protected abstract void init();
+    protected abstract void update(float delta);
+}
