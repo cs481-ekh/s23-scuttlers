da1aa601ac1fdd99d8bc6287341b7f8a7802e052
Kyle Hanson <khanson1029@gmail.com>
2023-03-07 11:22:19 -0700

    [#18] 150 create a split screen with drag and drop for ai editor (#152)
    * Added 'Root', 'Move', and 'Attack' button classes and Drag and Drop basic functionality
    
    * button and screen changes
    
    * button and screen changes

---
+++ antscuttle/core/src/com/antscuttle/game/Buttons/NewAIButton.java
@@ -0,0 +1,34 @@
+package com.antscuttle.game.Buttons;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class NewAIButton extends Button {
+
+    private final int WIDTH = 200;
+    private final int HEIGHT = 100;
+    
+    private final Texture INACTIVE = new Texture("buttons/New-AI.png");
+    private final Texture ACTIVE = new Texture("buttons/New-AI-Active.png");
+
+	@Override
+	public int getWidth() {
+		return WIDTH;
+	}
+	@Override
+	public int getHeight() {
+		return HEIGHT;
+	}
+    @Override
+    public Texture active() {
+        return ACTIVE;
+    }
+    @Override
+    public Texture inactive() {
+        return INACTIVE;
+    }
+    @Override
+    public String getButtonType() {
+        return ButtonType.newai.toString();
+    }
+    
+}
