ed699ad055b15eb2bd5a8d9fe5f56fb5de927379
Jack Cunningham <49688146+Jackcunningolf@users.noreply.github.com>
2023-03-31 19:07:32 -0600

    [#187] interact block execute (#217)

--- antscuttle/core/src/com/antscuttle/game/AI/implementations/InteractBlock.java
+++ antscuttle/core/src/com/antscuttle/game/AI/implementations/InteractBlock.java
@@ -5,8 +5,10 @@ import com.antscuttle.game.AI.BlockOptions;
 import com.antscuttle.game.AI.DecisionBlock;
 import com.antscuttle.game.AI.options.InteractOptions;
 import com.antscuttle.game.Ant.Ant;
+import com.antscuttle.game.Ant.BaseAnt;
 import com.antscuttle.game.Level.LevelData;
 import com.antscuttle.game.Util.GameData;
+import com.badlogic.gdx.ai.pfa.Graph;
 
 /**
  *
@@ -22,9 +24,20 @@ public class InteractBlock extends DecisionBlock{
     
     @Override
     public void execute(GameData gameData, LevelData levelData){
+        // get the current ant 
+        // BaseAnt ant = (BaseAnt) gameData.getCurrentAnt();
+
+        // get the graph
+        // Graph g = levelData.getLevelGraph(ant.getIntelligence())
+
+        // get the ant coords
+        // Point interactCoords = ant.getCoords();
         
-        super.execute(gameData, levelData);
-        // target.interact()?
+        // get the levelObject from the graph
+        // LevelObject target = g.getLevelObjectAtCoords(interactCoords)
+
+        // interact with the levelObject
+        // target.interact();
     }
     public static Class<? extends BlockOptions> getOptionsClass(){
         return InteractOptions.class;
