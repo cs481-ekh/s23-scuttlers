eccf0dbeac5a6a7a0b86311d3df5b8a81c186c09
anthonyoffire <anthonybaird@protonmail.com>
2023-04-15 11:37:44 -0600

    completed rendering level2, added end tiles and pressure plate animation

--- antscuttle/core/src/com/antscuttle/game/LevelObject/implementations/PressurePlate.java
+++ antscuttle/core/src/com/antscuttle/game/LevelObject/implementations/PressurePlate.java
@@ -6,6 +6,7 @@ import com.antscuttle.game.Damage.DamageType;
 import com.antscuttle.game.Level.LevelData;
 import com.antscuttle.game.LevelObject.InteractableLevelObject;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /**
@@ -16,15 +17,25 @@ public class PressurePlate extends InteractableLevelObject{
 
     private InteractableLevelObject affectedObject;
     private boolean isPressed;
+    private TextureRegion anim[];
+    private int pressedTime;
     
-    public PressurePlate(TextureRegion tex, InteractableLevelObject o){
-        super(tex, 0, 0);
+    public PressurePlate(TextureRegion tex[], InteractableLevelObject o){
+        super(tex[0], 0, 0);
+        this.anim = tex; 
         this.affectedObject = o;
+        isPressed = false;
+        pressedTime = 0;
     }
     
     @Override
     public boolean interact(Ant ant, LevelData levelData) {
-        return affectedObject.interact(ant, levelData);
+        if(!isPressed){
+            //affectedObject.interact(ant, levelData);
+            isPressed = true;
+            sprite.setRegion(anim[1]);
+        }
+        return true;
     }
 
     @Override
@@ -35,7 +46,15 @@ public class PressurePlate extends InteractableLevelObject{
     @Override
     protected void init() {
     }
-
-    
-    
+    @Override
+    public void update(float delta){
+        super.update(delta);
+        if(isPressed){
+            if(pressedTime++ == 40){
+                pressedTime = 0;
+                isPressed = false;
+                sprite.setRegion(anim[0]);
+            }
+        }
+    }
 }
--- antscuttle/core/src/com/antscuttle/game/Util/TileUtils.java
+++ antscuttle/core/src/com/antscuttle/game/Util/TileUtils.java
@@ -19,4 +19,14 @@ public class TileUtils {
         }
         return trimmed;
     }
+    public static TextureRegion[][] getPressurePlateTextures(){
+        TextureRegion allTiles = new TextureRegion(new Texture("animations/levelobj/pressure_plate.png"));
+        TextureRegion[][] trimmed = allTiles.split(17, 16);
+        for (TextureRegion[] row : trimmed) {
+            for (int j = 0; j<trimmed[0].length; j++) {
+                row[j] = new TextureRegion(row[j], 0, 0, 16, 16);
+            }
+        }
+        return trimmed;
+    }
 }
