703c4b0fac2db93dbfa2b4a7b8765090bcc7d61c
Jack Cunningham <49688146+Jackcunningolf@users.noreply.github.com>
2023-04-20 16:23:01 -0600

    [#218] simple implementation (#283)

--- antscuttle/core/src/com/antscuttle/game/AI/implementations/InteractBlock.java
+++ antscuttle/core/src/com/antscuttle/game/AI/implementations/InteractBlock.java
@@ -4,9 +4,11 @@ package com.antscuttle.game.AI.implementations;
 import com.antscuttle.game.AI.BlockOptions;
 import com.antscuttle.game.AI.DecisionBlock;
 import com.antscuttle.game.AI.options.InteractOptions;
+import com.antscuttle.game.AI.options.MoveOptions;
 import com.antscuttle.game.Ant.Ant;
 import com.antscuttle.game.Ant.BaseAnt;
 import com.antscuttle.game.Level.LevelData;
+import com.antscuttle.game.LevelObject.InteractableLevelObject;
 import com.antscuttle.game.Util.GameData;
 import com.badlogic.gdx.ai.pfa.Graph;
 
@@ -22,7 +24,10 @@ public class InteractBlock extends DecisionBlock{
     }
     
     @Override
+
     public void execute(GameData gameData, LevelData levelData, Ant dbOwner){
+
+        MoveBlock moveBlock = new MoveBlock(new MoveOptions(options.getFirstOptionChoice()));
         // get the current ant 
         // BaseAnt ant = (BaseAnt) gameData.getCurrentAnt();
 
@@ -37,6 +42,22 @@ public class InteractBlock extends DecisionBlock{
 
         // interact with the levelObject
         // target.interact();
+        
+        
+        if(!moveBlock.isFinished()) {
+            moveBlock.execute(gameData, levelData) ;
+        }  else {
+            // Successfully traveled to target
+            if (moveBlock.getExecutionResult()) {
+                InteractableLevelObject ilo = (InteractableLevelObject) moveBlock.objectTarget;
+                ilo.interact(gameData.getCurrentAnt(), levelData);
+                setFinished(true);
+                setExecutionResult(true);
+            }  else { // Could not travel to target
+                setFinished(true);
+                setExecutionResult(false);
+            }
+        }
     }
     public static Class<? extends BlockOptions> getOptionsClass(){
         return InteractOptions.class;
